9ea8c8ef1ae4db83dd0122c254716ee8c2143fae
Diego HR <dhernandez2448@gmail.com>
2024-11-01 09:28:35 -0400

    feat: change output

--- hello.java
+++ hello.java
@@ -5,13 +5,9 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
-
-       // delete this line
-       System.out.println("Modify this code");
-	
-       // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-
+       String firstName = "Diego";
+       String result = "Hello, I am " + firstName;
+       System.out.println(result);
     }
 }
 
